09b4b03cf39782645fda418521e82a2e9d596ab4
Arthur Camine <arthur.camine@gestaosat.com>
2020-09-20 21:21:14 -0300

    Finalização NAC01

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO users (name, email, pass) VALUES
 ('Joao Carlos', 'joao@gmail.com', 'o8seis*&ûshfs9');
+
+
+INSERT INTO tasks (title, description, point, status) VALUES
+('Edição de tasks', 'Editar tasks', 95, 50);
